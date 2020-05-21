25cc609b2e8fb0843814518bf94f8eb3e9174989
Nathaniel Perry <nathaniel.r.perry@gmail.com>
2020-05-21 16:45:29 -0500

    Added middleware utility

---
+++ util/middleware.js
@@ -0,0 +1,22 @@
+class Middleware {
+    constructor() {
+        this.stack = [];
+        this.step = 0;
+    }
+    
+    use(fn) {
+        this.stack.push(fn);
+    }
+
+    next() {
+        if (this.step < this.stack.length) {
+            this.stack[this.step++](this.hook, this.next.bind(this));
+        }
+    }
+    
+    run(hook) {
+        this.hook = hook || {};
+        this.step = 0;
+        this.next();
+    }
+}
