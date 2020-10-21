9e0eaf11aeed03624b5886f5368d164263d1989f
Nathaniel Perry <nathaniel.r.perry@gmail.com>
2020-10-21 17:33:24 -0500

    Added post command

---
+++ modules/commands/post.js
@@ -0,0 +1,26 @@
+const CommandList = require('../CommandList');
+
+const commands = [
+    {
+        name: 'post',
+        description: 'Post to the group-bulletin channel',
+        run(message, ...content) {
+            const guild = message.channel.guild;
+            const targetChannel = guild.channels.get(this.options.channelId);
+            if (targetChannel) {
+                //If channel exists, send content.
+                targetChannel.send(`--- <@${message.author.id}> posted the following bulletin:\n` + content.join(' '));
+                message.delete();
+            } else {
+                //If not, give error response.
+                throw Error(`${this.options.channelId} is not an existing text channel for ${guild.name}.`);
+            }
+        }
+    },
+];
+
+module.exports = class PostCommand extends CommandList {
+    constructor(options) {
+        super(commands, options);
+    }
+}
